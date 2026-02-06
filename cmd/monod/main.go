5e8df861867d74a26cdea8ce898673e32d78c2ec
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2026-02-06 00:16:44 -0500

    Initial commit

---
+++ cmd/monod/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	clienthelpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/monolythium/mono-chain/app"
+	"github.com/monolythium/mono-chain/cmd/monod/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clienthelpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
